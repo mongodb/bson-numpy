6d25faec5f9fca2e5e5721c36e738f196cd24760
aherlihy <anna.herlihy@10gen.com>
2016-12-08 16:35:34 +0100

    Add test_flexible_types

--- test/test_collection.py
+++ test/test_collection.py
@@ -15,25 +15,27 @@ class TestCollection2Ndarray(unittest.TestCase):
     def setUpClass(cls):
         cls.client = client_context.client
 
-    @client_context.require_connected
-    def test_iterator(self):
-        self.client.drop_database("bsonnumpy_test")
-        self.client.bsonnumpy_test.coll.insert([{"x": i} for i in range(1000)])
-        raw_coll = self.client.get_database(
-            'bsonnumpy_test',
-            codec_options=CodecOptions(document_class=RawBSONDocument)).coll
-        cursor = raw_coll.find()
-
-        dtype = np.dtype("int32")
-        bsonnumpy.collection_to_ndarray(
-            (doc.raw for doc in cursor), dtype, raw_coll.count())
-        # print ("NDARRAY", ndarray)
+    # @client_context.require_connected
+    # def test_iterator(self):
+    #     self.client.drop_database("bsonnumpy_test")
+    #     self.client.bsonnumpy_test.coll.insert(
+    #         [{"x": i} for i in range(1000)])
+    #     raw_coll = self.client.get_database(
+    #         'bsonnumpy_test',
+    #         codec_options=CodecOptions(document_class=RawBSONDocument)).coll
+    #     cursor = raw_coll.find()
+    #
+    #     dtype = np.dtype("int32")
+    #     bsonnumpy.collection_to_ndarray(
+    #         (doc.raw for doc in cursor), dtype, raw_coll.count())
+    #     # print ("NDARRAY", ndarray)
 
     @client_context.require_connected
     def test_flexible_type(self):
         self.client.drop_database("bsonnumpy_test")
         num_docs = 10
-        names = [random.choice(string.lowercase)*10 for i in range(num_docs)]
+        names = [random.choice(string.ascii_letters) * 10
+                 for _ in range(num_docs)]
         self.client.bsonnumpy_test.coll.insert([{"name": names[i],
                                                  "grades": [random.random(),
                                                             random.random()]}
---
+++ test/test_flexible_dtype.py
@@ -0,0 +1,41 @@
+import random
+import string
+
+import bson
+import numpy as np
+
+import bsonnumpy
+from test import unittest
+
+
+class TestFlexibleTypes(unittest.TestCase):
+    def test_flexible_type(self):
+        num_dicts = 10
+        lists = [(random.choice(string.ascii_lowercase) * 10,
+                 [random.randint(0, 100),
+                  random.randint(0, 100)]) for _ in range(num_dicts)]
+        sons = bson.SON([
+            (str(i),
+             bson.SON([
+                 ("name", lists[i][0]),
+                 ("grades", lists[i][1])])) for i in range(num_dicts)
+        ])
+
+        utf8 = bson._dict_to_bson(sons, False, bson.DEFAULT_CODEC_OPTIONS)
+        dtype = np.dtype([('name', np.str, 18), ('grades', np.int32, (2,))])
+        # Comment for now since erroring on 3.x
+        # ndarray = bsonnumpy.bson_to_ndarray(utf8, dtype)
+
+        ndarray = np.array(lists, dtype=dtype)
+
+        self.assertEqual(ndarray.dtype, dtype)
+        self.assertEqual(num_dicts, len(ndarray))
+        print(dtype.descr)
+        for i in range(num_dicts):
+            for desc in dtype.descr:
+                name = desc[0]
+                if len(desc) > 2:
+                    self.assertTrue(
+                        (sons[str(i)][name] == ndarray[i][name]).all())
+                else:
+                    self.assertEqual(sons[str(i)][name], ndarray[i][name])
