4889e132228ee1208110059aef4e004d50255174
aherlihy <anna.herlihy@10gen.com>
2016-12-08 17:58:38 +0100

    Add test for when length-1 arrays stay arrays

--- test/test_arrays.py
+++ test/test_arrays.py
@@ -84,6 +84,8 @@ class TestFromBSONArrays(TestFromBSON):
         for b in range(len(result)):
             self.assertTrue(np.array_equal(document[str(b)], result[b]))
 
+    def test_3d_len1(self):
+        # arrays of length 1 are maintained when they are within another array
         document = bson.SON([("0",
                               [[[9], [9]],
                                [[8], [8]],
@@ -98,6 +100,39 @@ class TestFromBSONArrays(TestFromBSON):
                                [[1], [1]]])])
         utf8 = bson._dict_to_bson(document, False, bson.DEFAULT_CODEC_OPTIONS)
         dtype = np.dtype('(3,2,1)int32')
+        result = bsonnumpy.bson_to_ndarray(utf8, dtype)
+        self.assertEqual(dtype.subdtype[0], result.dtype)
+        for b in range(len(result)):
+            self.assertTrue(np.array_equal(document[str(b)], result[b]))
+
+        document = bson.SON([("0", [[9], [8], [7]]),
+                             ("1", [[6], [5], [4]]),
+                             ("2", [[3], [2], [1]])])
+        utf8 = bson._dict_to_bson(document, False, bson.DEFAULT_CODEC_OPTIONS)
+        dtype = np.dtype('(3,1)int32')
+        result = bsonnumpy.bson_to_ndarray(utf8, dtype)
+        self.assertEqual(dtype.subdtype[0], result.dtype)
+        for b in range(len(result)):
+            self.assertTrue(np.array_equal(document[str(b)], result[b]))
+
+        expected_document = bson.SON([("0", [7, 7]),
+                                      ("1", [4, 4]),
+                                      ("2", [1, 1])])
+        document = bson.SON([("0", [[7, 7]]),
+                             ("1", [[4, 4]]),
+                             ("2", [[1, 1]])])
+        utf8 = bson._dict_to_bson(document, False, bson.DEFAULT_CODEC_OPTIONS)
+        dtype = np.dtype('2int32')
+        result = bsonnumpy.bson_to_ndarray(utf8, dtype)
+        self.assertEqual(dtype.subdtype[0], result.dtype)
+        for b in range(len(result)):
+            self.assertTrue(np.array_equal(expected_document[str(b)], result[b]))
+
+        document = bson.SON(
+            [("0", [[[[99]]], [[[88]]]])])
+        utf8 = bson._dict_to_bson(
+            document, False, bson.DEFAULT_CODEC_OPTIONS)
+        dtype = np.dtype("(2,1,1,1)int32")
         result = bsonnumpy.bson_to_ndarray(utf8, dtype)
         self.assertEqual(dtype.subdtype[0], result.dtype)
         for b in range(len(result)):
