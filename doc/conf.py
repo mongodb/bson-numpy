ce6f31f4bc4eeb7b2b5eb7634abfffbd536db0d7
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-01-16 10:31:52 -0500

    don't require sphinx.ext.githubpages

--- doc/conf.py
+++ doc/conf.py
@@ -30,9 +30,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.githubpages']
+extensions = [
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
