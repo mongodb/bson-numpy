3b59013348c7281e2f6cfb98121360ee820c2e31
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-06-16 06:52:31 -0400

    update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,7 @@ import timeit
 
 import pymongo
 import numpy as np
-from bson import CodecOptions
+from bson import BSON, CodecOptions, Int64
 from bson.raw_bson import RawBSONDocument
 
 import bsonnumpy
@@ -13,10 +13,12 @@ N_DOCUMENTS = 10 * 1000
 N_TRIALS = 50
 
 db = None
+raw_bson = None
 
 
 def _setup():
     global db
+    global raw_bson
 
     db = pymongo.MongoClient().bsonnumpy_test
     collection = db.collection
@@ -24,6 +26,14 @@ def _setup():
     collection.insert_many([
         {'_id': i, 'x': i * math.pi}
         for i in range(N_DOCUMENTS)])
+
+    # Ignore for now that the first batch defaults to 101 documents.
+    raw_bson_docs = [{'_id': i, 'x': float(i)} for i in range(1000)]
+    raw_bson = BSON.encode({'ok': 1,
+                            'cursor': {
+                                'id': Int64(1234),
+                                'ns': 'db.collection',
+                                'firstBatch': raw_bson_docs}})
 
 
 def _teardown():
@@ -52,14 +62,27 @@ def conventional_func():
 @bench('bson-numpy')
 def bson_numpy_func():
     raw_coll = db.get_collection(
-            'collection',
-            codec_options=CodecOptions(document_class=RawBSONDocument))
+        'collection',
+        codec_options=CodecOptions(document_class=RawBSONDocument))
 
     cursor = raw_coll.find()
     dtype = np.dtype([('_id', np.int64), ('x', np.float64)])
-
     bsonnumpy.sequence_to_ndarray(
         (doc.raw for doc in cursor), dtype, raw_coll.count())
+
+
+@bench('bson')
+def bson_func():
+    for _ in BSON(raw_bson).decode()['cursor']['firstBatch']:
+        pass
+
+
+@bench('raw-bson')
+def raw_bson_func():
+    options = CodecOptions(document_class=RawBSONDocument)
+    for _ in BSON(raw_bson).decode(options)['cursor']['firstBatch']:
+        pass
+
 
 _setup()
 
@@ -71,7 +94,7 @@ for name in sys.argv[1:]:
 
 for name, fn in bench_fns.items():
     if not sys.argv[1:] or name in sys.argv[1:]:
-        duration = timeit.timeit(bson_numpy_func, number=N_TRIALS)
-        print("%s: %.2f" % (name, duration / N_TRIALS))
+        duration = min(timeit.Timer(fn).repeat(3, N_TRIALS))
+        print("%s: %.2f" % (name, duration))
 
 _teardown()
