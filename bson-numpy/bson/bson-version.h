c355e51585ca955cf0bdb0d8b6bcf9e366abc3fa
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-06-28 16:03:27 -0400

    generate bson-version.h for libbson 1.6.3
    #21

--- bson-numpy/bson/bson-version.h
+++ bson-numpy/bson/bson-version.h
@@ -37,7 +37,7 @@
  *
  * BSON minor version component (e.g. 2 if %BSON_VERSION is 1.2.3)
  */
-#define BSON_MINOR_VERSION (7)
+#define BSON_MINOR_VERSION (6)
 
 
 /**
@@ -45,7 +45,7 @@
  *
  * BSON micro version component (e.g. 3 if %BSON_VERSION is 1.2.3)
  */
-#define BSON_MICRO_VERSION (0)
+#define BSON_MICRO_VERSION (3)
 
 
 /**
@@ -53,14 +53,14 @@
  *
  * BSON prerelease version component (e.g. rc0 if %BSON_VERSION is 1.2.3-rc0)
  */
-#define BSON_PRERELEASE_VERSION (dev)
+#define BSON_PRERELEASE_VERSION ()
 
 /**
  * BSON_VERSION:
  *
  * BSON version.
  */
-#define BSON_VERSION (1.7.0-dev)
+#define BSON_VERSION (1.6.3)
 
 
 /**
@@ -69,7 +69,7 @@
  * BSON version, encoded as a string, useful for printing and
  * concatenation.
  */
-#define BSON_VERSION_S "1.7.0-dev"
+#define BSON_VERSION_S "1.6.3"
 
 
 /**
