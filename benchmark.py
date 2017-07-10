63d8a93e839a3e4349b4a9252676c0312cb34e01
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-07-10 11:43:15 -0400

    more benchmark options

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,6 @@
+import argparse
 import collections
 import math
-import os
 import string
 import sys
 import timeit
@@ -15,19 +15,14 @@ import bsonnumpy
 
 assert pymongo.has_c()
 
-if 'BSONNUMPY_TEST_BENCHMARK' in os.environ:
-    N_LARGE_DOCS = 2
-    N_SMALL_DOCS = 2
-    N_TRIALS = 1
-else:
-    N_LARGE_DOCS = 10
-    N_SMALL_DOCS = 100000
-    N_TRIALS = 5
 
+# Use large document in tests? If SMALL, no, if LARGE, then yes.
+SMALL = False
+LARGE = True
 db = None
 raw_bson = None
 large_doc_keys = None
-collection_names = {True: "large", False: "small"}
+collection_names = {LARGE: "large", SMALL: "small"}
 dtypes = {}
 raw_bsons = {}
 
@@ -38,7 +33,7 @@ def _setup():
     global large_doc_keys
 
     db = pymongo.MongoClient().bsonnumpy_test
-    small = db[collection_names[False]]
+    small = db[collection_names[SMALL]]
     small.drop()
 
     print("%d small docs, %d bytes each with 3 keys" % (
@@ -49,9 +44,9 @@ def _setup():
         {'x': 1, 'y': math.pi}
         for _ in range(N_SMALL_DOCS)])
 
-    dtypes[False] = np.dtype([('x', np.int64), ('y', np.float64)])
+    dtypes[SMALL] = np.dtype([('x', np.int64), ('y', np.float64)])
 
-    large = db[collection_names[True]]
+    large = db[collection_names[LARGE]]
     large.drop()
     # 2600 keys: 'a', 'aa', 'aaa', .., 'zz..z'
     large_doc_keys = [c * i for c in string.ascii_lowercase
@@ -62,7 +57,7 @@ def _setup():
 
     large.insert_many([large_doc.copy() for _ in range(N_LARGE_DOCS)])
 
-    dtypes[True] = np.dtype([(k, np.float64) for k in large_doc_keys])
+    dtypes[LARGE] = np.dtype([(k, np.float64) for k in large_doc_keys])
 
     # Ignore for now that the first batch defaults to 101 documents.
     raw_bson_docs_small = [{'x': 1, 'y': math.pi} for _ in range(N_SMALL_DOCS)]
@@ -79,8 +74,8 @@ def _setup():
                                       'ns': 'db.collection',
                                       'firstBatch': raw_bson_docs_large}})
 
-    raw_bsons[False] = raw_bson_small
-    raw_bsons[True] = raw_bson_large
+    raw_bsons[SMALL] = raw_bson_small
+    raw_bsons[LARGE] = raw_bson_large
 
 
 def _teardown():
@@ -139,6 +134,12 @@ def raw_bson_func(use_large):
     bsonnumpy.sequence_to_ndarray(batches, dtype, c.count())
 
 
+@bench('parse-dtype')
+def raw_bson_func(use_large):
+    dtype = dtypes[use_large]
+    bsonnumpy.sequence_to_ndarray([], dtype, 0)
+
+
 @bench('decoded-cmd-reply')
 def bson_func(use_large):
     for _ in BSON(raw_bsons[use_large]).decode()['cursor']['firstBatch']:
@@ -152,26 +153,61 @@ def raw_bson_func(use_large):
         pass
 
 
-_setup()
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                 epilog="""
+Available benchmark functions:
+   %s
+""" % ("\n   ".join(bench_fns.keys()),))
+parser.add_argument('--large', action='store_true',
+                    help='only test with large documents')
+parser.add_argument('--small', action='store_true',
+                    help='only test with small documents')
+parser.add_argument('--test', action='store_true',
+                    help='quick test of benchmark.py')
+parser.add_argument('funcs', nargs='*', default=bench_fns.keys())
+options = parser.parse_args()
 
-for name in sys.argv[1:]:
+
+if options.test:
+    N_LARGE_DOCS = 2
+    N_SMALL_DOCS = 2
+    N_TRIALS = 1
+else:
+    N_LARGE_DOCS = 1000
+    N_SMALL_DOCS = 100000
+    N_TRIALS = 5
+
+# Run tests with both small and large documents.
+sizes = [SMALL, LARGE]
+if options.large and not options.small:
+    sizes.remove(SMALL)
+if options.small and not options.large:
+    sizes.remove(LARGE)
+
+for name in options.funcs:
     if name not in bench_fns:
         sys.stderr.write("Unknown function \"%s\"\n" % name)
         sys.stderr.write("Available functions:\n%s\n" % ("\n".join(bench_fns)))
         sys.exit(1)
 
+_setup()
 
+print()
 print("%25s: %7s %7s" % ("BENCH", "SMALL", "LARGE"))
 
 for name, fn in bench_fns.items():
-    if not sys.argv[1:] or name in sys.argv[1:]:
+    if name in options.funcs:
         sys.stdout.write("%25s: " % name)
         sys.stdout.flush()
 
         # Test with small and large documents.
-        for use_large in False, True:
-            duration = min(timeit.Timer(partial(fn, use_large)).repeat(3, N_TRIALS))
-            sys.stdout.write("%7.2f " % duration)
+        for size in (SMALL, LARGE):
+            if size not in sizes:
+                sys.stdout.write("%7s" % "-")
+            else:
+                timer = timeit.Timer(partial(fn, size))
+                duration = min(timer.repeat(3, N_TRIALS)) / float(N_TRIALS)
+                sys.stdout.write("%7.2f " % duration)
             sys.stdout.flush()
 
         sys.stdout.write("\n")
