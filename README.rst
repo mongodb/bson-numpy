5305a7b976af659f6a9ff566a287294c7cea7c65
Bernie Hackett <bernie@10gen.com>
2019-09-17 16:03:32 -0400

    Fix metadata and add required files and headers
    This also sets the min required CPython version to 3.5
    and the min NumPy version to 1.17.0.
    
    Resolves #29
    Resolves #44
    Resolves #57
    Resolves #58
    Resolves #59

---
+++ README.rst
@@ -0,0 +1,65 @@
+==========
+BSON-NumPy
+==========
+:Info: See `the mongo site <http://www.mongodb.org>`_ for more information. See `GitHub <http://github.com/mongodb/bson-numpy>`_ for the latest source.
+:Author: Anna Herlihy
+:Maintainer: The MongoDB Python Team
+:Documentation: https://bson-numpy.readthedocs.io/en/latest/
+
+About
+=====
+
+A Python extension written in C that uses `libbson
+<http://mongoc.org/libbson/current>`_ to convert between `NumPy<https://pypi.org/project/numpy/>`_
+arrays and `BSON<http://bsonspec.org>`_, the native data format of MongoDB.
+
+This project is a **prototype** and not ready for production use.
+
+Bugs / Feature Requests
+=======================
+
+Think you’ve found a bug? Want to see a new feature in BSON-NumPy? `Please open a
+issue in github.<https://github.com/mongodb/bson-numpy/issues>`_
+
+How To Ask For Help
+-------------------
+
+Please include all of the following information when opening an issue:
+
+- Detailed steps to reproduce the problem, including full traceback, if possible.
+- The exact python version used, with patch level::
+
+  $ python -c "import sys; print(sys.version)"
+
+- The operating system and version (e.g. Windows 7, OSX 10.8, ...)
+
+Installation
+============
+
+Please see the `instructions on readthedocs.
+<https://bson-numpy.readthedocs.io/en/latest/#installing>`_
+
+Dependencies
+============
+
+BSON-NumPy supports CPython 3.5+. BSON-NumPy requires NumPy 1.17.0+.
+
+Examples
+========
+
+Please see the `examples on readthedocs.
+<https://bson-numpy.readthedocs.io/en/latest/#converting-mongodb-data-to-numpy>`_
+
+Documentation
+=============
+
+Please see the `documentation on readthedocs.
+<https://bson-numpy.readthedocs.io/en/latest/>`_
+
+Testing
+=======
+
+The easiest way to run the tests is to run **python setup.py test** in
+the root of the distribution.
+
+.. _sphinx: http://sphinx.pocoo.org/
--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ import setuptools
 from setuptools.command.build_ext import build_ext as _build_ext
 
 
+with open('README.rst') as f:
+    readme_content = f.read()
+
 # See http://stackoverflow.com/questions/19919905, we need to install NumPy
 # during setup before building bsonnumpy.so, then use its get_include().
 class build_ext(_build_ext):
@@ -42,15 +45,9 @@ bsonnumpymodule = setuptools.Extension(
     libraries=libraries)
 
 
-if sys.version_info[:2] == (2, 6):
-    # NumPy 1.12 dropped Python 2.6.
-    setup_requires = ["numpy==1.11.2"]
-    tests_require = ["pymongo", "unittest2"]
-    test_suite = "unittest2.collector"
-else:
-    setup_requires = ["numpy>=1.11.0"]
-    tests_require = ["pymongo"]
-    test_suite = "test"
+setup_requires = ["numpy>=1.17.0"]
+tests_require = ["pymongo>=3.9.0"]
+test_suite = "test"
 
 
 # For developing BSON-NumPy with CLion and CMake, useful to disable setuptools.
@@ -60,12 +57,27 @@ if 'BSONNUMPY_DISABLE_BUILD' not in os.environ:
 
 setuptools.setup(
     name='BSON-NumPy',
-    version='0.1',
-    description='Module for converting directly from BSON to NumPy ndarrays'
-                ' and vice versa',
+    version='0.2.0.dev0',
+    description='Module for converting directly from BSON to NumPy ndarrays',
+    long_description=readme_content,
     author='Anna Herlihy',
-    author_email='anna@mongodb',
-    url='https://github.com/aherlihy/bson-numpy',
+    author_email='mongodb-user@googlegroups.com',
+    url='https://github.com/mongodb/bson-numpy',
+    keywords=["mongo", "mongodb", "pymongo", "numpy", "bson"],
+    license="Apache License, Version 2.0",
+    python_requires=">=3.5",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Database"],
     ext_modules=ext_modules,
     test_suite=test_suite,
     tests_require=tests_require,
