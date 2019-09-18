056d9c74f9b5bf516cc97ebb5c25f7cd001bbf1a
Prashant Mital <prashantmital@gmail.com>
2019-09-18 11:45:34 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     pass
 
-from setuptools import setup, Command, Extension
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 
 
